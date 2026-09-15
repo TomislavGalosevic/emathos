a8c812d4237040182d555adfec6c71056e3c4547
TomislavGalosevic <tgalosevic@gmail.com>
2026-09-15 23:18:34 +0200

    Add Render frontend domain to CORS allow_origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,9 +8,17 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="e-Mathos API", version="0.3.0")
 
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://emathos-frontend.onrender.com"
+    "https://emathos-1.onrender.com", 
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
